2c275b242ec8e42784fbb6e442401729a4a36193
samrat1699 <samratdev151@gmail.com>
2023-09-12 18:16:13 +0600

    Model tranier

--- src/mlprojects/components/data_ingestion.py
+++ src/mlprojects/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.mlprojects.components.data_transformation import DataTransformation
 from src.mlprojects.components.data_transformation import DataTransformationConfig
 
-#from src.mlprojects.components.model_tranier import ModelTrainerConfig
-#from src.mlprojects.components.model_tranier import ModelTrainer
+from src.mlprojects.components.model_tranier import ModelTrainerConfig
+from src.mlprojects.components.model_tranier import ModelTrainer
 
 
 @dataclass
@@ -60,14 +60,12 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    #obj.initiate_data_ingestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    #train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
-        #train_data, test_data)
-    
-    data_transformation.initiate_data_transformation(
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data)
-   # modeltrainer = ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
